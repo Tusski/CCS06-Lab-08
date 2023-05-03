2853b71aba9f4072b28959e3fe49db27410b59e6
Tusski <aalmalel@yahoo.com>
2023-05-03 22:49:01 +0800

    Final version

--- register.php
+++ register.php
@@ -11,29 +11,54 @@
 <form action="save-registration.php" method="POST">
 	<div>
 		<label>First Name</label>
-		<input type="text" name="first_name" placeholder="First Name" />	
+		<input type="text" name="first_name" required />	
+	</div>
+	<div>
+		<label>Middle Name</label>
+		<input type="text" name="middle_name" />	
 	</div>
 	<div>
 		<label>Last Name</label>
-		<input type="text" name="last_name" placeholder="Last Name" />	
+		<input type="text" name="last_name" required />	
 	</div>
 	<div>
 		<label>Email Address</label>
-		<input type="email" name="email" placeholder="email@address.com" />	
+		<input type="email" name="email" required />	
 	</div>
 	<div>
 		<label>Password</label>
-		<input type="password" name="password" />	
+		<input type="password" name="password" required />	
 	</div>
 	<div>
 		<label>Confirm Password</label>
-		<input type="password" name="confirm_password" />	
+		<input type="password" name="confirm_password" required />	
 	</div>
+	<div>
+		<label>Birthdate</label>
+		<input type="date" name="birthdate" required />	
+	</div>
+	<div>
+		<label>Gender:</label>
+		<input type="radio" name="gender" value="male" required />
+		<label>Male</label>
+		<input type="radio" name="gender" value="female" />
+		<label>Female</label>
+	</div><br>
+	<div>
+		<label>Address</label>
+		<textarea name="address" required></textarea>	
+	</div>
+	<div>
+		<label>Contact Number</label>
+		<input type="tel" name="contact_number" required />	
+	</div><br>
 	<div>
 		<button>
 			Register User
 		</button>	
 	</div>
 </form>
+
+
 </body>
 </html>
--- src/User.php
+++ src/User.php
@@ -6,144 +6,150 @@ use PDO;
 
 class User
 {
-	protected $id;
-	protected $first_name;
-	protected $last_name;
-	protected $email;
-	protected $pass;
-	protected $created_at;
+    protected $id;
+    protected $first_name;
+    protected $middle_name;
+    protected $last_name;
+    protected $email;
+    protected $password;
+    protected $birthdate;
+    protected $gender;
+    protected $address;
+    protected $contact_number;
+    protected $created_at;
 
-	public function getId()
-	{
-		return $this->id;
-	}
+    public function getId()
+    {
+        return $this->id;
+    }
 
-	public function getFullName()
-	{
-		return $this->first_name . ' ' . $this->last_name;
-	}
+    public function getFullName()
+    {
+        return $this->first_name . ' ' . $this->middle_name . ' ' . $this->last_name;
+    }
 
-	public function getFirstName()
-	{
-		return $this->first_name;
-	}
+    public function getFirstName()
+    {
+        return $this->first_name;
+    }
 
-	public function getLastName()
-	{
-		return $this->last_name;
-	}
+    public function getMiddleName()
+    {
+        return $this->middle_name;
+    }
 
-	public function getEmail()
-	{
-		return $this->email;
-	}
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
 
-	public static function getById($id)
-	{
-		global $conn;
+    public function getEmail()
+    {
+        return $this->email;
+    }
 
-		try {
-			$sql = "
-				SELECT * FROM users
-				WHERE id=:id
-				LIMIT 1
-			";
-			$statement = $conn->prepare($sql);
-			$statement->execute([
-				'id' => $id
-			]);
-			$result = $statement->fetchObject('App\User');
-			return $result;
-		} catch (PDOException $e) {
-			error_log($e->getMessage());
-		}
+    public static function getById($id)
+    {
+        global $conn;
 
-		return null;
-	}
+        try {
+            $sql = "
+                SELECT * FROM users
+                WHERE id=:id
+                LIMIT 1
+            ";
+            $statement = $conn->prepare($sql);
+            $statement->execute([
+                'id' => $id
+            ]);
+            $result = $statement->fetchObject('App\User');
+            return $result;
+        } catch (PDOException $e) {
+            error_log($e->getMessage());
+        }
 
-	public static function hashPassword($password)
-	{
-		$hashed_password = null;
-		// DO SOMETHING HERE TO HASH THE PASSWORD
-		// ...
-		return $hashed_password;
-	}
+        return null;
+    }
 
-	public static function attemptLogin($email, $pass)
-	{
-		global $conn;
+    public static function hashPassword($password)
+    {
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        return $hashed_password;
+    }
 
-		try {
-			$sql = "
-				SELECT * FROM users
-				WHERE email=:email
-					AND pass=:pass
-				LIMIT 1
-			";
-			$statement = $conn->prepare($sql);
+    public static function attemptLogin($email, $password)
+    {
+        global $conn;
 
-			// Perform password hash verification (if necessary)
+        try {
+            $sql = "
+                SELECT * FROM users
+                WHERE email=:email
+                LIMIT 1
+            ";
+            $statement = $conn->prepare($sql);
 
-			$statement->execute([
-				'email' => $email,
-				'pass' => $pass
-			]);
-			$result = $statement->fetchObject('App\User');
-			return $result;
-		} catch (PDOException $e) {
-			error_log($e->getMessage());
-		}
+            $statement->execute([
+                'email' => $email,
+            ]);
+            $user = $statement->fetchObject('App\User');
 
-		return null;
-	}
+            if ($user && password_verify($password, $user->password)) {
+                return $user;
+            }
+        } catch (PDOException $e) {
+            error_log($e->getMessage());
+        }
 
-	public static function register($first_name, $last_name, $email, $password)
-	{
-		global $conn;
+        return null;
+    }
 
-		try {
-			// Hash the password before inserting it to DB
-			// ..
+    public static function register($first_name, $middle_name, $last_name, $email, $password, $birthdate, $gender, $address, $contact_number)
+    {
+        global $conn;
 
-			$sql = "
-				INSERT INTO users (first_name, last_name, email, pass)
-				VALUES ('$first_name', '$last_name', '$email', '$password')
-			";
-			$conn->exec($sql);
-			// echo "<li>Executed SQL query " . $sql;
-			return $conn->lastInsertId();
-		} catch (PDOException $e) {
-			error_log($e->getMessage());
-		}
+        try {
+            $hashed_password = self::hashPassword($password);
 
-		return false;
-	}
+            $sql = "
+                INSERT INTO users (first_name, middle_name, last_name, email, password, birthdate, gender, address, contact_number)
+                VALUES ('$first_name', '$middle_name', '$last_name', '$email', '$hashed_password', '$birthdate', '$gender', '$address', '$contact_number')
+            ";
+            $conn->exec($sql);
+
+            return $conn->lastInsertId();
+        } catch (PDOException $e) {
+            error_log($e->getMessage());
+        }
+
+        return false;
+    }
 
 	public static function registerMany($users)
-	{
-		global $conn;
+    {
+        global $conn;
 
-		try {
-			foreach ($users as $user) {
-				// Hash the password before inserting it to DB
-				// ..
+        try {
+            foreach ($users as $user) {
+                $hashed_password = self::hashPassword($user['password']);
 
-				$sql = "
-					INSERT INTO users
-					SET
-						first_name=\"{$user['first_name']}\",
-						last_name=\"{$user['last_name']}\",
-						email=\"{$user['email']}\",
-						pass=\"{$user['pass']}\"
-				";
-				$conn->exec($sql);
-				// echo "<li>Executed SQL query " . $sql;
-			}
-			return true;
-		} catch (PDOException $e) {
-			error_log($e->getMessage());
-		}
+                $sql = "
+                    INSERT INTO users
+                    SET
+                        first_name=\"{$user['first_name']}\",
+                        middle_name=\"{$user['middle_name']}\",
+                        last_name=\"{$user['last_name']}\",
+                        email=\"{$user['email']}\",
+                        password=\"{$hashed_password}\"
+                ";
+                $conn->exec($sql);
+            }
 
-		return false;
-	}
+            return true;
+        } catch (PDOException $e) {
+            error_log($e->getMessage());
+        }
+
+        return false;
+    }
 }
